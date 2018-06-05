4c65babd7e4bdc592d9b0b7902467a1014fb8209
Diego Kazadi Kalunda <diegokazadi@gmail.com>
2018-06-05 06:14:44 -0300

    Atualizando annotationPadrão

--- Seminario/Anotacoes/src/br/unifacisa/si/anotacaopadrao/FuncionarioOverride.java
+++ Seminario/Anotacoes/src/br/unifacisa/si/anotacaopadrao/FuncionarioOverride.java
@@ -22,16 +22,36 @@ public class FuncionarioOverride {
 		return builder.toString();
 		
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(salario);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
-		
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FuncionarioOverride other = (FuncionarioOverride) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
 
 }
